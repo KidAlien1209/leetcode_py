3591f1fa135178c22b8946327607accb11795086
amd <wanghao_ecpkn@163.com>
2019-06-28 00:17:42 +0800

    226 not finished

---
+++ 226.invert-binary-tree.py
@@ -0,0 +1,73 @@
+#
+# @lc app=leetcode id=226 lang=python3
+#
+# [226] Invert Binary Tree
+#
+# https://leetcode.com/problems/invert-binary-tree/description/
+#
+# algorithms
+# Easy (58.52%)
+# Likes:    1763
+# Dislikes: 28
+# Total Accepted:    333.9K
+# Total Submissions: 570.4K
+# Testcase Example:  '[4,2,7,1,3,6,9]'
+#
+# Invert a binary tree.
+# 
+# Example:
+# 
+# Input:
+# 
+# 
+# ⁠    4
+# ⁠  /   \
+# ⁠ 2     7
+# ⁠/ \   / \
+# 1   3 6   9
+# 
+# Output:
+# 
+# 
+# ⁠    4
+# ⁠  /   \
+# ⁠ 7     2
+# ⁠/ \   / \
+# 9   6 3   1
+# 
+# Trivia:
+# This problem was inspired by this original tweet by Max Howell:
+# 
+# Google: 90% of our engineers use the software you wrote (Homebrew), but you
+# can’t invert a binary tree on a whiteboard so f*** off.
+# 
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if root==None:
+            return None
+
+
+    def inStack(self, stack, res):
+        while stack!= []:
+            item = stack.pop()
+            res.append(item)
+            if item.left!= None and item.right!=None:
+                stack.append(item.right)
+                stack.append(item.left) 
+
+    def outStack(self, root, stack):
+        root = stack.pop(0)
+        while stack!=[]:
+            root.left=stack.pop()
+
+
+
+
